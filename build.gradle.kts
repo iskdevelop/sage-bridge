6d007d6a8905eaa0bf94c36d58e7a4315a1a7388
Kid A <thisismeamir@outlook.com>
2025-05-28 11:39:25 +0000

    [init]

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,51 +1,28 @@
-// build.gradle.kts
+
 plugins {
-    kotlin("jvm") version "1.9.10"
-    kotlin("plugin.serialization") version "1.9.10"
-    application
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.ktor)
+    alias(libs.plugins.kotlin.plugin.serialization)
 }
 
-group = "com.iskportal.sagebridge"
-version = "1.0.0"
+group = "con.iskportal"
+version = "0.0.1"
+
+application {
+    mainClass = "io.ktor.server.netty.EngineMain"
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    val ktorVersion = "2.3.4"
-    val kotlinxSerializationVersion = "1.6.0"
-    val logbackVersion = "1.4.11"
-    
-    // Ktor server dependencies
-    implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-server-netty-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-server-cors-jvm:$ktorVersion")
-    
-    // Serialization
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$kotlinxSerializationVersion")
-    
-    // Logging
-    implementation("ch.qos.logback:logback-classic:$logbackVersion")
-    
-    // Testing
-    testImplementation("io.ktor:ktor-server-tests-jvm:$ktorVersion")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.9.10")
-}
-
-application {
-    mainClass.set("com.iskportal.sagebridge.ApplicationKt")
-}
-
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions {
-        jvmTarget = "11"
-    }
-}
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    implementation(libs.ktor.server.content.negotiation)
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.ktor.server.netty)
+    implementation(libs.logback.classic)
+    implementation(libs.ktor.server.config.yaml)
+    testImplementation(libs.ktor.server.test.host)
+    testImplementation(libs.kotlin.test.junit)
 }
